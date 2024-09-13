1de8bb0f50bbfb4d25c4f903e61fbb79c73b6acb
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2024-09-13 08:51:02 -0300

    ajustes gerais

---
+++ executar-projeto.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo ""
+echo "Subindo containers..."
+docker compose -f docker-compose-frontend-backend.yml up -d
+echo "Containers gerado!"
+
+echo ""
+read -p "Pressione enter para encerrar e aguarde um tempinho para que os containers iniciem completamente...Visite o arquivo README.md com instruções de uso do projeto."
